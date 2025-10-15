863f6d0037eec065184daccbaa8b321164860bbd
mrismaildev <ismail.dev.khulna@gmail.com>
2025-10-15 10:22:10 +0600

    added Total price

--- script/script.js
+++ script/script.js
@@ -2,21 +2,25 @@ const plusBtn = document.querySelector('.plus');
 const minusBtn = document.querySelector('.minus');
 const quantity = document.querySelector('.quantity');
 const itemPrice = document.querySelector('.item-price');
+const price = document.querySelector('.price');
 
-const basePrice = parseFloat(itemPrice.textContent.replace('$', ''))
-let count = parseInt(quantity.textContent)
-
+const basePrice = parseFloat(itemPrice.textContent.replace('$', ''));
+let count = parseInt(quantity.textContent);
 function updateItemPrice() {
-  const total = basePrice * count
-  itemPrice.textContent=`$${total.toFixed(2)}`
+  const total =basePrice*count
+  itemPrice.textContent = `$${total.toFixed(2)}`;
+  updateTotalPrice(total)
+}
+
+function updateTotalPrice(total) {
+  // Total price এলিমেন্টে মান বসাও
+  price.textContent = `$${total.toFixed(2)}`;
 }
 
 plusBtn.addEventListener('click', () => {
   count++;
   quantity.textContent = count;
-  updateItemPrice()
- 
-
+  updateItemPrice();
 });
 
 minusBtn.addEventListener('click', () => {
@@ -24,5 +28,5 @@ minusBtn.addEventListener('click', () => {
     count--;
   }
   quantity.textContent = count;
-  updateItemPrice()
+  updateItemPrice();
 });
